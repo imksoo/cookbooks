970a1859ba0b78efdd918a652385bc778268761c
Kirino Minato <kajiya.takeshi@gmail.com>
2013-03-22 14:52:15 +0900

    Add zabbix server rpms from EPEL.

--- zabbix-server/recipes/default.rb
+++ zabbix-server/recipes/default.rb
@@ -6,3 +6,59 @@
 #
 # All rights reserved - Do Not Redistribute
 #
+[
+ ["cad60abafcf8d8e9845ffe9df1b500869b1d0b49c4cccfbe5acbbe2d6a8ee39c","libxslt-1.1.26-2.el6_3.1.x86_64.rpm"],
+ ["df6704b3c6ff34915069a3478fbabf37c13a570474979f06365e7f83f2a9bad6","zabbix20-2.0.5-1.el6.x86_64.rpm"],
+ ["491afb04d08e571abf3d2e363b2c47b6a50d1bb626360f616cc6da0653740efe","php-pdo-5.3.3-22.el6.x86_64.rpm"],
+ ["258fdb64c609a4e1007ef707529351c9223e6250fb2f6aba1093cb099ce0f396","libX11-1.5.0-4.el6.x86_64.rpm"],
+ ["1e851358d60908703d1526c92d06be93eab983a2d984437a5047a20552534bd1","net-snmp-libs-5.5-44.el6.x86_64.rpm"],
+ ["b30e8943859ebacfe3a17a0bc974a73406ab2a1e644315a93bdf6892f76a36fd","php-bcmath-5.3.3-22.el6.x86_64.rpm"],
+ ["ebea2066b8032410c764113cdf292d4badc09209f083121d85c74b5d4139f0f0","php-5.3.3-22.el6.x86_64.rpm"],
+ ["96af2669a82750164bbbabe767501e0005b3aa1728abc191e7d579870e6a08fa","OpenIPMI-libs-2.0.16-14.el6.x86_64.rpm"],
+ ["d1fdc6dd60b48cf1fca4e1364bf24bbcbf10df2fdb10edb6e59517461a3c89a5","lm_sensors-libs-3.1.1-17.el6.x86_64.rpm"],
+ ["d0a58ddeae0df842fa4b43eb83a143c85a129eb9b44ea797401a4d9f27d6c460","libXau-1.0.6-4.el6.x86_64.rpm"],
+ ["e39a5df65977c3a8701b62646c4ff10abbcc21f1238a29b463d3e9770edf2c5c","dejavu-fonts-common-2.30-2.el6.noarch.rpm"],
+ ["6fa37b8f1ff2dabec810dd59e5b1de60660187755725d9de5f303fbfd8eb0366","apr-1.3.9-5.el6_2.x86_64.rpm"],
+ ["9e041701c90fa348b8c66793a472ef9ba616714b2e95414d52cf90e5fa03bada","mailcap-2.1.31-2.el6.noarch.rpm"],
+ ["ea3aba4a2c5734accd67a3da4ba967168ad78d30cb744bc6379863878c594022","libpng-1.2.49-1.el6_2.x86_64.rpm"],
+ ["11a1820f3029bc428afbcc29532caef36194464690c5f6b13f29243f2d84de43","libxcb-1.8.1-1.el6.x86_64.rpm"],
+ ["9c1f1b80c275c8806f1edd5be6334932920a5ca94f07150f6ac8bb2844959fa5","libtool-ltdl-2.2.6-15.5.el6.x86_64.rpm"],
+ ["366add909d7879a10ba0f7d4ad00604b064f8fec5d33d44f64589582c5a4a9d3","zabbix20-web-2.0.5-1.el6.noarch.rpm"],
+ ["28d1327a857658673cd18ec0bb888781a8ccbd85e5697a0dcb805cdc5257bf67","libX11-common-1.5.0-4.el6.noarch.rpm"],
+ ["1b4256dc8536652956b51a0d72abe3390dc34f8d68b87d3b1652dc9baa5fbda9","traceroute-2.0.14-2.el6.x86_64.rpm"],
+ ["50685e513f3c78edef04ae72f2c5cb829bbdc9cfef8f8d9280e2ab5efb8178ed","zabbix20-server-2.0.5-1.el6.x86_64.rpm"],
+ ["b1034ad949913604df5b25540b0ffc4e66e62ca17ece23d2a551d029d66fa7e2","php-common-5.3.3-22.el6.x86_64.rpm"],
+ ["2e2c5a1e6d0bc357f745d686f5788843636a52e1940103b5dfd553a4a2e708d8","libXpm-3.5.10-2.el6.x86_64.rpm"],
+ ["258ab0783abc9c99984baaacb9f402d11d4f435f785efb2e12a9e32c8915f5c8","fping-2.4b2-10.el6.x86_64.rpm"],
+ ["21befa0ede7fa142528fc7e3ab06923526a01136cf92646979bb256f52334504","php-mbstring-5.3.3-22.el6.x86_64.rpm"],
+ ["de9c43e809f96d15d8b580deca232508b3e5992e7474b014979e82a8a7513ab3","apr-util-1.3.9-3.el6_0.1.x86_64.rpm"],
+ ["15b063e47a143bf61aef300af45ae6a443dfc33f093724a7880c3dd3e8860432","php-gd-5.3.3-22.el6.x86_64.rpm"],
+ ["9ba9f36686d404a510251fc59d39f528416e66834404f1685eb11791196e06c3","gnutls-2.8.5-10.el6_4.1.x86_64.rpm"],
+ ["272befed08f246ab3bb967060c861242b6517e9cc239245185990bc0f5b31402","unixODBC-2.2.14-12.el6_3.x86_64.rpm"],
+ ["829622a7612aebe8190d1a38068ed6270441574320f0c792a927fd4e5e3c5ec5","libtasn1-2.3-3.el6_2.1.x86_64.rpm"],
+ ["cb88af7b7aa9a4e5ced0b2df1bd303052d78bf8bfcf86c681b5553181816f4b3","fontpackages-filesystem-1.41-1.1.el6.noarch.rpm"],
+ ["e968826ecfe4c724a62946a2ee320e36938041094a30f99f2b4a71f01dae4eff","php-mysql-5.3.3-22.el6.x86_64.rpm"],
+ ["0ba55b5fd3f1be53709c67884f8c75bd16916277a9f40327648de0619ad25b14","freetype-2.3.11-14.el6_3.1.x86_64.rpm"],
+ ["e7b384178b393eb70bf6bff81d5ceac72e32421dbf3241503e159bb65e33cf78","httpd-tools-2.2.15-26.el6.centos.x86_64.rpm"],
+ ["2eaf3e0976becbc6d51f9d200a8ddf983f847dedd41ae12157a6bf1e9f397d4a","dejavu-sans-fonts-2.30-2.el6.noarch.rpm"],
+ ["736c1c82a92ce410a66b4adf698244d51b88805c5815ee9715960cbc4e740895","php-xml-5.3.3-22.el6.x86_64.rpm"],
+ ["8b01e287ba2d2dd13ed43482dfc833dc90fa7c759a52f86c64db738e6d1ab0c1","apr-util-ldap-1.3.9-3.el6_0.1.x86_64.rpm"],
+ ["e24791e043ac7b18aeef8bddac9f68b6d39b6645d779aa669b5a0f5d609536e4","httpd-2.2.15-26.el6.centos.x86_64.rpm"],
+ ["4fd3fea85f8ae1d9f1c2311cc6120d4bf50f0e1999a7258e334d481c02bf1f8b","libjpeg-turbo-1.2.1-1.el6.x86_64.rpm"],
+ ["5170e81ae3d87a1f8e982c417af72834e0108d86bbbb6ba76e46d0d303af078d","iksemel-1.4-2.el6.x86_64.rpm"],
+ ["d8963645128b01b0e54691f29137ca7297fcf16b16f60744282e497dc90e7aa3","php-cli-5.3.3-22.el6.x86_64.rpm"],
+ ["18c0659cbdfad2bacb6a36951d93c0c277122ca8ab2d1d7b0e24c8421216fffd","zabbix20-web-mysql-2.0.5-1.el6.noarch.rpm"],
+ ["1a2d8deb06fc2b3571ed6473f9b3c572e842ee42b42865b8f1b712274793fec6","zabbix20-server-mysql-2.0.5-1.el6.x86_64.rpm"]
+].each do |checksum, rpmfile|
+ cookbook_file "/tmp/#{rpmfile}" do
+  source "#{rpmfile}"
+  checksum "#{checksum}"
+ end
+ 
+ package `rpm -q --qf=%{NAME} -p #{rpmfile}` do
+  action :install
+  source "/tmp/#{rpmfile}"
+  provider Chef::Provider::Package::Rpm
+ end
+end
+
